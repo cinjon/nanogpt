26deffb68f05f9d471b5f8f9ada5ac00b9f64515
Cinjon Resnick <cinjon.resnick@gmail.com>
2023-12-24 17:15:01 -0500

    Test some of sepcualtive sampling.

--- helpers/test_sampling.py
+++ helpers/test_sampling.py
@@ -14,6 +14,20 @@ def test_top_p():
     logits_top_p = sampling.top_p(logits, p=1)
     assert torch.all(logits_top_p == logits)
 
+    # Test that p=0 throws an error.
+    try:
+        fails = sampling.top_p(logits, p=0)
+        assert False
+    except ValueError as e:
+        pass
+
+    # Test that p>1 throws an error.
+    try:
+        fails = sampling.top_p(logits, p=5)
+        assert False
+    except ValueError as e:
+        pass
+
     # Test that the right logits are returned masked when p = 0.9.
     np_logits = np.array([[[0.95, 0.05, 0., 0.], [0.1, 0.2, 0.3, 0.4], [0.2, 0.4, 0.05, 0.35]], 
                           [[0.0, 0.05, 0.91, 0.04], [0.4, 0.3, 0.22, 0.08], [0.2, 0.4, 0.05, 0.35]]])
@@ -31,8 +45,53 @@ def test_beam_search():
     pass
 
 
-def test_speculative_sampling():
-    pass
+def test_speculative_sampling():    
+    # Test that the accepts are created correctly.
+    torch.manual_seed(0)
+    torch.cuda.manual_seed(0)    
+    target_probs = np.array([
+        [0.1, 0.2, 0.3, 0.5, 0.8],
+        [0.2, 0.4, 0.05, 0.95, 0.]
+    ])
+    draft_probs = np.array([
+        [0.09, 0.25, 0.5, 0.9, 0.7],
+        [0.1, 0.4, 0.01, 0.99, 0.5]
+    ])
+    # At this seed, random float is:
+    random_uniform = np.array(
+        [[0.4963, 0.7682, 0.0885, 0.1320, 0.3074],
+       [0.6341, 0.4901, 0.8964, 0.4556, 0.6323]]
+    )
+    accepts = [
+        [target_probs[i, j] / draft_probs[i, j] > random_uniform[i, j] for j in range(5)] for i in range(2)
+    ]
+    accepts = np.array(accepts)
+
+    draft_probs = torch.from_numpy(draft_probs)
+    target_probs = torch.from_numpy(target_probs)
+    expected = torch.from_numpy(accepts)
+
+    accepts = sampling._get_speculative_sampling_accepts(target_probs, draft_probs)
+    assert torch.all(accepts == expected)
+
+    # Test that the (target - draft)_+ distro is created correctly.
+    target_probs = np.array([[[0.95, 0.05, 0., 0.], [0.1, 0.2, 0.3, 0.4], [0.2, 0.4, 0.05, 0.35]], 
+                        [[0.0, 0.05, 0.91, 0.04], [0.4, 0.3, 0.22, 0.08], [0.2, 0.4, 0.05, 0.35]]])
+    draft_probs = np.array([[[0.95, 0.05, 0., 0.], [0.2, 0.25, 0.2, 0.35], [0.2, 0.4, 0.05, 0.35]], 
+                        [[0.0, 0.05, 0.91, 0.04], [0.4, 0.3, 0.22, 0.08], [0.05, 0.4, 0.15, 0.40]]])
+    first_rejection = np.array([1, 2])
+    target_at_rejection = np.array([[0.1, 0.2, 0.3, 0.4], [0.2, 0.4, 0.05, 0.35]])
+    draft_at_rejection = np.array([[0.2, 0.25, 0.2, 0.35], [0.05, 0.4, 0.15, 0.40]])
+    minus = np.maximum(target_at_rejection - draft_at_rejection, 0)
+    minus = minus / minus.sum(axis=-1, keepdims=True)
+    expected = torch.from_numpy(minus)
+
+    target_probs = torch.from_numpy(target_probs)
+    draft_probs = torch.from_numpy(draft_probs)
+    first_rejection = torch.from_numpy(first_rejection)
+    distro = sampling._get_speculative_sampling_target_minus_draft_distribution(draft_probs, target_probs, first_rejection)
+
+    assert torch.all(distro == expected)
 
 
 if __name__ == "__main__":
